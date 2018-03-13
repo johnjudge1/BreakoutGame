2c0aa21f085ad1240e8782bbec9e1746727fa83c
johnjudge1 <35769354+johnjudge1@users.noreply.github.com>
2018-03-13 20:27:16 +0000

    Bounce off the walls

--- breakout.js
+++ breakout.js
@@ -1,14 +1,16 @@
 //Setup the canvas
 var canvas = document.getElementById("myCanvas");
+
+//the ctx variable sets up the 2d context so we can paint on it
 var ctx = canvas.getContext("2d");
 
-//Set the starting point
+//Setup other variables for the ball size and position
+var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 
-//Draw the ball
 function drawBall() {
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -18,9 +20,23 @@ function drawBall() {
 }
 
 function draw() {
+	//Clear the canvas
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	darwBall();
+	
+	//Draw the ball
+	drawBall();
+	
+	//Bounce off the walls
+	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+		dx = -dx;
+	}
+	
+	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+		dy = -dy;
+	}
+	
 	x += dx;
 	y += dy;
-
-setInterval(draw, 10);	
+}
+	
+setInterval(draw, 10);
