e8b012f8c7c236cb4fc7f1049b4541ac0906a7bb
johnjudge1 <35769354+johnjudge1@users.noreply.github.com>
2018-03-16 14:25:20 +0000

    Make the bricks disappear

--- js/breakout.js
+++ js/breakout.js
@@ -4,6 +4,9 @@ var canvas = document.getElementById("myCanvas");
 //the ctx variable sets up the 2d context so we can paint on it
 var ctx = canvas.getContext("2d");
 
+//Counting the score
+var score = 0;
+
 //Setup other variables for the ball size and position
 var ballRadius = 10;
 var x = canvas.width/2;
@@ -15,7 +18,7 @@ var ballColour = "#0095DD";
 //Now define the paddle
 var paddleHeight = 10;
 var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth)/2
+var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
@@ -28,12 +31,35 @@ var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 
+//Moniter the documents for events that move the paddle
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
 //Hold the bricks in a two-dimensional array - think of it as rows and columns
 var bricks = [];
 for(c=0; c<brickColumnCount; c++) {
 	bricks[c] = [];
 	for(r=0; r<brickRowCount; r++) {
 		bricks[c][r] = { x: 0, y: 0, status: 1 };
+	}
+}
+
+//Define the functions to handle the keyDown or keyUp events
+function keyDownHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = true;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = false;
 	}
 }
 
@@ -59,26 +85,51 @@ function drawPaddle(){
 function drawBricks() {
 	for(c=0; c<brickColumnCount; c++) {
 		for(r=0; r<brickRowCount; r++) {
-			var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-			var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-			bricks[c][r].x = brickX;
-			bricks[c][r].y = brickY;
-			ctx.beginPath();
-			ctx.rect(brickX, brickY, brickWidth, brickHeight);
-			ctx.fillStyle = "#0095DD";
-			ctx.fill();
-			ctx.closePath();
+			if(bricks[c][r].status == 1) {
+				var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+				var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+				bricks[c][r].x = brickX;
+				bricks[c][r].y = brickY;
+				ctx.beginPath();
+				ctx.rect(brickX, brickY, brickWidth, brickHeight);
+				ctx.fillStyle = "#0095DD";
+				ctx.fill();
+				ctx.closePath();
+			}
+		}
+	}
+
+}
+
+function collisionDetection() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			var b = bricks[c][r];
+			if(b.status == 1) {
+			    if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+				    dy = -dy;
+					b.status = 0;
+					score++;
+				}
+			}
 		}
 	}
 }
 
-
+//Counting the score
+function drawScore() {
+	ctx.font = "16px Arial";
+	ctx.fillStyle = "#0095DD";
+	ctx.fillText("Score: "+score, 8, 20);
+	document.getElementById("gamescore").innerHTML = "Score: " + score;
+}
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
 	drawPaddle();
 	collisionDetection();
+	drawScore();
 	drawBricks();
 	
 	//Bounce the ball off three walls - if it drops off the bottom - Game Over!
@@ -97,6 +148,7 @@ function draw() {
 		    document.location.reload();
 	    }   
 	}
+	
 	if(rightPressed && paddleX < canvas.width-paddleWidth){
 		paddleX += 7;
 	}
@@ -104,42 +156,10 @@ function draw() {
 		paddleX -= 7;
 	}
 	
+	
 	x += dx;
 	y += dy;
-}
-	
-//Moniter the documents for events that move the paddle
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
 
-//Define the functions to handle the keyDown or keyUp events
-function keyDownHandler(e) {
-	if(e.keyCode == 39) {
-		rightPressed = true;
-	}
-	else if(e.keyCode == 37) {
-		leftPressed = true;
-	}
 }
 
-function keyUpHandler(e) {
-	if(e.keyCode == 39) {
-		rightPressed = false;
-	}
-	else if(e.keyCode == 37) {
-		leftPressed = false;
-	}
-}
-
-function collisionDetection() {
-	for(c=0; c<brickColumnCount; c++) {
-		for(r=0; r<brickRowCount; r++) {
-			var b = bricks[c][r];
-			if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
-				dy = -dy;
-			}
-		}
-	}
-}
-
-setInterval(draw, 10);
+setInterval (draw, 10);
